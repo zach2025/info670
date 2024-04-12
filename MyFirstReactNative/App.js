cf46af70b046f8f96f26224d60dd5bc8f0e8562a
zach2025 <zachthomas2025@gmail.com>
2024-04-11 22:22:44 -0400

    Assignment 1 Project and Solution

---
+++ MyFirstReactNative/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is Zachary Thomas!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-This is a temporary README file
+#INFO 670 Assignments - Zachary Thomas
+
+Assignment 1
+![Assignment 1 Solution](<Screenshot 2024-04-11 at 10.16.38 PM.png>)
